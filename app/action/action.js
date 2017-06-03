86b1e8c1c67119a799384fe11ef18d5f8c06dc4f
xiazhiyu <xiazhiyu2010@126.com>
2017-06-03 14:50:54 +0800

    add begin to http

--- app/action/action.js
+++ app/action/action.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiKey = "c08b8cf5b5d78e5926a693e33fd29fc9";
-const baseURL = `http://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=metric`;
+const begin = "https://cors-anywhere.herokuapp.com/";
+const baseURL = `${begin}http://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=metric`;
 export const getWeather = (city) => {
   let url = `${baseURL}&q=${city},cn`;
   let result = axios.get(url);
